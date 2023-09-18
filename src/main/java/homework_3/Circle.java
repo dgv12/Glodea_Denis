6a29b8d1f5fff4d7b3ff84bed0aaeed7c49d2cfd
CompTerra <glodeadenis7@gmail.com>
2023-09-18 10:31:20 +0300

    Finish task Radius Circle and Temperature converter ...Verified please

---
+++ src/main/java/homework_3/Circle.java
@@ -0,0 +1,18 @@
+package homework_3;
+
+public class Circle {
+        double radius;
+        public Circle(float radius) {
+            this.radius = radius;
+        }
+        public double calculateArea(){
+            double pi=3.14159265;
+            return (pi*radius*radius);
+        }
+        public static void main(String[] args) {
+        Circle alpha = new Circle(3);
+        double area = alpha.calculateArea();
+        System.out.println(area);
+
+        }
+    }
